b534bd8356adc65b06ef0163301351f748eb427d
csabaialbert <csabaialbert@gmail.com>
2023-05-13 15:24:45 +0200

    cerated-models-and-db-migrations

---
+++ app/Models/Animal.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Animal extends Model
+{
+    protected $fillable = ['aname', 'species'];
+    public function connection()
+    {
+        return $this->hasMany(Connection::class, 'animalid');
+    }
+
+    
+}
---
+++ app/Models/Novel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Novel extends Model
+{
+    protected $fillable = ['pyear', 'title', 'publisher'];
+    public function connection()
+    {
+        return $this->belongsTo(Connection::class, 'novelid');
+    }
+}
